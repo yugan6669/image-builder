442fae2c73919d272880a13dbfa2d9acf2c082de
krishnamaram2 <48556990+krishnamaram2@users.noreply.github.com>
2020-07-25 02:19:43 +0530

    Update ssh-keys.sh

--- src/ssh-keys.sh
+++ src/ssh-keys.sh
@@ -1,4 +1,4 @@
 echo "hello"
-s#sh-keygen -q -t rsa -N '' -f /home/centos/.ssh/id_rsa <<<y 2>&1 >/dev/null
+#ssh-keygen -q -t rsa -N '' -f /home/centos/.ssh/id_rsa <<<y 2>&1 >/dev/null
 #cat /home/centos/.ssh/id_rsa.pub >> /home/centos/.ssh/authorized_keys
 #ssh -o StrictHostKeyChecking=no centos@localhost
